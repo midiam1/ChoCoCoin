6715b851d564426b8f6126ab5b0706db0f6af169
midiam1 <admhimmeros@gmail.com>
2022-01-13 13:23:55 -0400

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,33 @@
+    # Proyectos Himmeros
+    # Scripts de  librerías necesarias para compilar ChocoCoin
+
+sudo apt-get -y install build-essential libssl-dev libboost-all-dev
+
+    # Resuelve un error con OpenSSL ya que necesita una versión vieja
+
+sudo apt-get -y remove libssl-dev
+sudo apt -y install libssl1.0-dev
+
+sudo add-apt-repository -y ppa:bitcoin/bitcoin
+sudo apt-get -y update
+sudo apt-get install -y libdb4.8-dev libdb4.8++-dev
+sudo apt-get install -y libminiupnpc-dev
+
+    # Vamos a necesitar memoria, así que ampliamos la memoria virtual o swap file
+
+sudo swapoff /swapfile # Apago la memoria virtual
+sudo rm /swapfile      # Elimino la memorial virtual
+
+sudo dd if=/dev/zero of=/swapfile bs=1024 count=4194304 # Creo una memoria virtual más grande
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile && sudo swapon /swapfile
+
+    # Corrige un posible error de permisos
+
+chmod 755 ../src/leveldb/build_detect_platform
+
+    # Y limpio, para que no de errores
+
+cd leveldb
+make clean
+cd ..
