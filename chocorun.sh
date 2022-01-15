0f98aef2f4b44bd87f8e24e4c3fb2f6c9a6b14b5
midiam1 <admhimmeros@gmail.com>
2022-01-15 00:07:11 -0400

    Actualización
    Copia del sitio real de ChocoCoin ya que el fork no dejó colocarlo público .
    
    Se anexan las últimos modificaciones al código, como las direcciones IPs de los monederos activos para la fecha .

---
+++ chocorun.sh
@@ -0,0 +1,10 @@
+    # Acá empezamos a compilar
+    # Tanto demonio como Qt
+    # Proyectos Himmeros
+    # sistemas_que_funcionan_
+    # versión 0.1
+
+    # Ya debes tener git instalado para efectuar la clonación a lo
+    # Resident Evil ><
+
+    git clone 
